eaad3f78aff3089dd35b463f7c7791e27c851e12
Kamakura <barnett.yuxiang@gmail.com>
2024-05-31 11:56:44 +0800

    Floating Point Arithmetic

---
+++ devhub/projects/udemy_100_days_of_code_python_pro_bootcamp/day_2.py
@@ -0,0 +1,16 @@
+
+print(round(8 / 3, 3))
+
+print(8 // 3) # type int
+print(type(8 // 3)) # type int
+print(4 / 2) # type float
+
+# f-string
+score = 0
+height = 1.8
+isWinning = True
+print(f"Your score is {score}, your height is {height}, you are winning is {isWinning}")
+
+num = 0.1
+print(f'{num:.20f}')  # 使用更多的小数位数来显示近似值
+print(round(.3, 10))
