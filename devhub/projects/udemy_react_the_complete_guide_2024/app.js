077b593dd32028d4e5f4ea4312a6d0b2cbe482bc
Kamakura <barnett.yuxiang@gmail.com>
2024-06-13 11:24:47 +0800

    react course

---
+++ devhub/projects/udemy_react_the_complete_guide_2024/app.js
@@ -0,0 +1,23 @@
+// Class is rarely used
+class User {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  greet() {
+    console.log(`Hello, I am ${this.name}`);
+  }
+}
+
+const user = new User('Max', 30);
+console.log(user);
+
+
+// Arrays & Array Methods like map()
+hobbies = ['Sports', 'Cooking', 'Reading'];
+console.log(hobbies)
+hobbies.push('Programming');
+console.log(hobbies);
+
+
