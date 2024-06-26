c138eb5c93e5d3fe9c17321b5f2a435ef4db6418
Kamakura <barnett.yuxiang@gmail.com>
2024-06-26 16:32:58 +0800

    Setting HTML Attributes Dynamically & Loading Image FilesS

--- devhub/projects/udemy_react_the_complete_guide_2024/s03/src/App.jsx
+++ devhub/projects/udemy_react_the_complete_guide_2024/s03/src/App.jsx
@@ -1,3 +1,5 @@
+import reactImg from './assets/react-core-concepts.png';
+
 const reactDescription = ["Fundamental", "Crucial", "Core"];
 function genRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
@@ -7,7 +9,7 @@ function Header() {
   const description = reactDescription[genRandomInt(2)];
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
         {description} React concepts you will need for almost any app you are
