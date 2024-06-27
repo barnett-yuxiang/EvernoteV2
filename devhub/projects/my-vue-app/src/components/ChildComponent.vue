28706650a3b2ccf2bd8e9b780757766086a1c56f
Kamakura <barnett.yuxiang@gmail.com>
2024-06-27 18:37:23 +0800

    Showcasing some notable features of Vue.js

---
+++ devhub/projects/my-vue-app/src/components/ChildComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <p>Child Component: Count is {{ count }}</p>
+    <button @click="reset">Reset Count</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ChildComponent',
+  props: {
+    count: Number
+  },
+  methods: {
+    reset() {
+      this.$emit('reset');
+    }
+  }
+}
+</script>
+
+<style scoped>
+button {
+  margin-top: 10px;
+}
+</style>
