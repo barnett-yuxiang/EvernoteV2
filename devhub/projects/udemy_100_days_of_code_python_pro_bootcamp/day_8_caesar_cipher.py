ccb989a22850d921d5bd714f15e7be96b5535ba8
Kamakura <barnett.yuxiang@gmail.com>
2024-07-02 15:55:12 +0800

    Refactor caesar cipher code and add encryption and decryption functions

--- devhub/projects/udemy_100_days_of_code_python_pro_bootcamp/day_8_caesar_cipher.py
+++ devhub/projects/udemy_100_days_of_code_python_pro_bootcamp/day_8_caesar_cipher.py
@@ -4,23 +4,32 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-#Don't change the code above 👆
-
-#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
 def encrypt(plain_text, shift_amount):
-  #TODO-2: Inside the encrypt function, shift each letter of the text forwards in the alphabet by the shift amount and print the encrypted text.  
-  #e.g. 
-  #plain_text = "hello"
-  #shift = 5
-  #cipher_text = "mjqqt"
-  #print output: "The encoded text is mjqqt"
   cipher_text = ""
   for letter in plain_text:
     position = alphabet.index(letter)
     new_position = position + shift_amount
-    new_letter = alphabet[new_position]
-    cipher_text += new_letter
+    cipher_text += alphabet[new_position]
   print(f"The encoded text is {cipher_text}")
 
-#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
-encrypt(plain_text=text, shift_amount=shift)
+#TODO-1: Create a different function called 'decrypt' that takes the 'text' and 'shift' as inputs.
+def decrypt(cipher_text, shift_amount):
+  #TODO-2: Inside the 'decrypt' function, shift each letter of the 'text' *backwards* in the alphabet by the shift amount and print the decrypted text.  
+  #e.g. 
+  #cipher_text = "mjqqt"
+  #shift = 5
+  #plain_text = "hello"
+  #print output: "The decoded text is hello"
+  plain_text = ""
+  for letter in cipher_text:
+    position = alphabet.index(letter)
+    new_position = position - shift_amount
+    plain_text += alphabet[new_position]
+  print(f"The decoded text is {plain_text}")
+
+
+#TODO-3: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. Then call the correct function based on that 'drection' variable. You should be able to test the code to encrypt *AND* decrypt a message.
+if direction == "encode":
+  encrypt(plain_text=text, shift_amount=shift)
+elif direction == "decode":
+  decrypt(cipher_text=text, shift_amount=shift)
