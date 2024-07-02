26ed8db894383a5be7e2cb2e4efe585d31d8ac3e
Kamakura <barnett.yuxiang@gmail.com>
2024-07-02 22:03:25 +0800

    dictionary

---
+++ devhub/projects/udemy_100_days_of_code_python_pro_bootcamp/day_9.py
@@ -0,0 +1,5 @@
+programming_dictionary = {
+    "Bug": "An error in a program that prevents the program from running as expected.",
+    "Function": "A piece of code that you can easily call over and over again."
+}
+
