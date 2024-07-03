5a5cc9e9092c7e1dc06971c73f2bcd2ac9387559
Kamakura <barnett.yuxiang@gmail.com>
2024-07-03 23:00:10 +0800

    Refactor App component to use map function for rendering CoreConcept components

--- devhub/projects/udemy_react_the_complete_guide_2024/s03/src/App.jsx
+++ devhub/projects/udemy_react_the_complete_guide_2024/s03/src/App.jsx
@@ -35,14 +35,9 @@ function App() {
         <section id="core-concepts">
           <h2>Time to get started!</h2>
           <ul>
-            <CoreConcept
-              title={CORE_CONCEPTS[0].title}
-              description={CORE_CONCEPTS[0].description}
-              image={CORE_CONCEPTS[0].image}
-            />
-            <CoreConcept {...CORE_CONCEPTS[1]} />
-            <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
+            {CORE_CONCEPTS.map((conceptItem) => (
+              <CoreConcept {...conceptItem} />
+            ))}
           </ul>
         </section>
         <section id="examples">
