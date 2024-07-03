0b711dabeee5f50d440151ff7ec041607d413bc0
Kamakura <barnett.yuxiang@gmail.com>
2024-07-03 14:51:25 +0800

    feat: Add TabButton component for examples section

--- devhub/projects/udemy_react_the_complete_guide_2024/s03/src/App.jsx
+++ devhub/projects/udemy_react_the_complete_guide_2024/s03/src/App.jsx
@@ -2,6 +2,7 @@ import componetsImg from "./assets/components.png";
 import { CORE_CONCEPTS } from "./data.js";
 import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
+import TabButton from "./components/TabButton.jsx";
 
 function App() {
   return (
@@ -20,6 +21,12 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[2]} />
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+          </menu>
         </section>
       </main>
     </div>
