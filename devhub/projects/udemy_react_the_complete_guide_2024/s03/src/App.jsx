510de0bc8beb288faef80ab3b8f0672308ebc80f
Kamakura <barnett.yuxiang@gmail.com>
2024-07-03 22:34:54 +0800

    Refactor App component to handle selected topic in state and improve TabButton component

--- devhub/projects/udemy_react_the_complete_guide_2024/s03/src/App.jsx
+++ devhub/projects/udemy_react_the_complete_guide_2024/s03/src/App.jsx
@@ -15,6 +15,19 @@ function App() {
     console.log(selectedTopic);
   }
 
+  let tabContent = <p>Please select a topic.</p>;
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
+  }
+
   return (
     <div>
       <Header />
@@ -42,16 +55,7 @@ function App() {
             <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
             <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
-          {!selectedTopic && <p>Please select a topic.</p>}
-          {selectedTopic ? (
-            <div id="tab-content">
-              <h3>{EXAMPLES[selectedTopic].title}</h3>
-              <p>{EXAMPLES[selectedTopic].description}</p>
-              <pre>
-                <code>{EXAMPLES[selectedTopic].code}</code>
-              </pre>
-            </div>
-          ) : null}
+          {tabContent}
         </section>
       </main>
     </div>
