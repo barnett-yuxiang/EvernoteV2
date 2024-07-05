2e3c9e35c1d30e742e26f8dbf022c6ecb5c3af3d
Kamakura <barnett.yuxiang@gmail.com>
2024-07-05 15:06:17 +0800

    refactor: Update player display in App component

--- devhub/projects/udemy_react_the_complete_guide_2024/s04_2/src/App.jsx
+++ devhub/projects/udemy_react_the_complete_guide_2024/s04_2/src/App.jsx
@@ -7,12 +7,18 @@ function App() {
       <div id="game-container">
         <ol id="players">
           <li>
-            <span className="player-name">Player 1</span>
-            <span className="player-symbol">X</span>
+            <span className="player">
+              <span className="player-name">Player 1</span>
+              <span className="player-symbol">X</span>
+            </span>
+            <button>Edit</button>
           </li>
           <li>
-            <span className="player-name">Player 2</span>
-            <span className="player-symbol">O</span>
+            <span className="player">
+              <span className="player-name">Player 2</span>
+              <span className="player-symbol">O</span>
+            </span>
+            <button>Edit</button>
           </li>
         </ol>
         GAME BOARD
