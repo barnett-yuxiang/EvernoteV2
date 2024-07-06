3b702c28f9c4a51990097f70bd1f08a02e2708d4
Kamakura <barnett.yuxiang@gmail.com>
2024-07-06 23:46:14 +0800

    refactor: Update player display in App component

--- devhub/projects/udemy_react_the_complete_guide_2024/s04_2/src/App.jsx
+++ devhub/projects/udemy_react_the_complete_guide_2024/s04_2/src/App.jsx
@@ -1,31 +1,27 @@
+import { useState } from "react";
+
+import Player from "./components/Player.jsx";
+import GameBoard from "./components/GameBoard.jsx";
 
 function App() {
-  
+  const [activePlayer, setActivePlayer] = useState("X");
+
+  function handleSelectSquare() {
+    setActivePlayer((player) => (player === "X" ? "O" : "X"));
+  }
 
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <li>
-            <span className="player">
-              <span className="player-name">Player 1</span>
-              <span className="player-symbol">X</span>
-            </span>
-            <button>Edit</button>
-          </li>
-          <li>
-            <span className="player">
-              <span className="player-name">Player 2</span>
-              <span className="player-symbol">O</span>
-            </span>
-            <button>Edit</button>
-          </li>
+        <ol id="players" className="highlight-player">
+          <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'} />
+          <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} />
         </ol>
-        GAME BOARD
+        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer} />
       </div>
       LOGO
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ devhub/projects/udemy_react_the_complete_guide_2024/s04_2/src/components/GameBoard.jsx
@@ -0,0 +1,33 @@
+export default function Gameboard({ onSelectSquare, activePlayerSymbol }) {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  function handleSelectSquare(rowIndex, colindex) {
+    setGameBoard((prevGameBoard) => {
+      const updatedBoard = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]),
+      ];
+      updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+      return updatedBoard;
+    });
+
+    onSelectSquare();
+  }
+
+  return (
+    <ol id="game-board">
+      {gameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+}
