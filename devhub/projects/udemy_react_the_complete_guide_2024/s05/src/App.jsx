a0b6b5e74f17408321cfed826c836d1c70f4dfb5
Kamakura <barnett.yuxiang@gmail.com>
2024-07-08 22:25:16 +0800

    refactor: Update App component to use Header and UserInput components

--- devhub/projects/udemy_react_the_complete_guide_2024/s05/src/App.jsx
+++ devhub/projects/udemy_react_the_complete_guide_2024/s05/src/App.jsx
@@ -2,11 +2,12 @@ import Header from "./components/Header";
 import UserInput from "./components/UserInput";
 
 function App() {
-  return;
-  <>
-    <Header />;
-    <UserInput />;
-  </>;
+  return (
+    <>
+      <Header />;
+      <UserInput />;
+    </>
+  );
 }
 
 export default App;
