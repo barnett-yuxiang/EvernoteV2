d3ab4aac547ef80dcd64e477b41f6ed33d2dc9e7
Kamakura <barnett.yuxiang@gmail.com>
2024-07-08 23:21:54 +0800

    refactor: Update App component to use controlled inputs in UserInput component

--- devhub/projects/udemy_react_the_complete_guide_2024/s05/src/App.jsx
+++ devhub/projects/udemy_react_the_complete_guide_2024/s05/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
     setUserInput((prevState) => {
       return {
         ...prevState,
-        [inputIdentifier]: newValue,
+        [inputIdentifier]: +newValue,
       };
     });
   }
--- devhub/projects/udemy_react_the_complete_guide_2024/s05/src/components/Results.jsx
+++ devhub/projects/udemy_react_the_complete_guide_2024/s05/src/components/Results.jsx
@@ -1,4 +1,9 @@
+import calculateInvestmentResults from "../util/investment.js";
+
 export default function Results({ input }) {
-  console.log(input);
+  const resultData = calculateInvestmentResults(input);
+
+  console.log(resultData);
+
   return <p>Results...</p>;
 }
